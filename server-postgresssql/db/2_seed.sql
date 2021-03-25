f3f7f259a4bb716a85791992fbbf97a0ba7bf300
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-25 13:02:40 +0000

    modified setup data

--- server-postgresssql/db/2_seed.sql
+++ server-postgresssql/db/2_seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO plants (name, light, weeks_kept_alive) 
 VALUES
-    ('Change Ivy', 'Not fussy', 0),
+    ('Devil Ivy', 'Not fussy', 0),
     ('Boston Fern','Lower light', 0),
     ('Aloe Vera','Bright light', 0);
    
